e8ea6efbefc40b44d718b06c7427b3ef24ff7c79
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-04-08 20:44:19 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini video BabyMonster");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini audio BabyMonster");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini gambar BabyMonster OT6");
+})
